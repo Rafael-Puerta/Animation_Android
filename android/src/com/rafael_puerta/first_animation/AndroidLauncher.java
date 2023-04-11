1f3e36af9bbe115c7e2bb258cb48e7825604ae4f
Rafael-Puerta <rafapc1992@gmail.com>
2023-04-11 16:53:11 +0200

    v1.0

---
+++ android/src/com/rafael_puerta/first_animation/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rafael_puerta.first_animation;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rafael_puerta.first_animation.firstAnimation;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new firstAnimation(), config);
+	}
+}
---
+++ ios/src/com/rafael_puerta/first_animation/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.rafael_puerta.first_animation;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.rafael_puerta.first_animation.firstAnimation;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new firstAnimation(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
