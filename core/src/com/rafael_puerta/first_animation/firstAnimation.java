a863d0725e62510e55f7266c1f93d8e7f6941c06
Rafael-Puerta <rafapc1992@gmail.com>
2023-04-18 17:00:02 +0200

    v3.1 with Websocket and animation move

--- core/src/com/rafael_puerta/first_animation/firstAnimation.java
+++ core/src/com/rafael_puerta/first_animation/firstAnimation.java
@@ -5,10 +5,13 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.czyzby.websocket.WebSocket;
 import com.github.czyzby.websocket.WebSocketListener;
@@ -21,7 +24,12 @@ public class firstAnimation extends ApplicationAdapter implements ApplicationLis
 	private static final int FRAME_COLS = 3, FRAME_ROWS = 1;
 	private WebSocket socket;
 	private String address = "localhost";
-	private int port = 8888;
+	private int port = 8888,posX=50;
+	private Rectangle up, down, left, right, fire;
+	private int SCR_WIDTH=800,SCR_HEIGHT=480;
+	private OrthographicCamera camera;
+	protected  int IDLE=0, UP=1, DOWN=2, LEFT=3, RIGHT=4;
+	private float lastSend = 0;
 	// Objects used
 	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
 	Texture walkSheet;
@@ -32,10 +40,15 @@ public class firstAnimation extends ApplicationAdapter implements ApplicationLis
 
 	@Override
 	public void create() {
-
+		camera=new OrthographicCamera();
+		camera.setToOrtho(false,800,480);
 		// Load the sprite sheet as a Texture
 		walkSheet = new Texture(Gdx.files.internal("beatRecortado.png"));
-
+		// facilities per calcular el "touch"
+		up = new Rectangle(0, SCR_HEIGHT*2/3, SCR_WIDTH, SCR_HEIGHT/3);
+		down = new Rectangle(0, 0, SCR_WIDTH, SCR_HEIGHT/3);
+		left = new Rectangle(0, 0, SCR_WIDTH/3, SCR_HEIGHT);
+		right = new Rectangle(SCR_WIDTH*2/3, 0, SCR_WIDTH/3, SCR_HEIGHT);
 		//TEST WEBSOCKET
 		if( Gdx.app.getType()== Application.ApplicationType.Android )
 			// en Android el host és accessible per 10.0.2.2
@@ -75,13 +88,26 @@ public class firstAnimation extends ApplicationAdapter implements ApplicationLis
 
 	@Override
 	public void render() {
+
+		int move= virtual_joystick_control();
+		if(move==LEFT){
+			posX += -1;
+		}else if (move==RIGHT){
+			posX += 1;
+		}
+
+		if( stateTime-lastSend > 1.0f ) {
+			lastSend = stateTime;
+			socket.send("Enviar dades");
+		}
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
 		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
 		// Get current frame of animation for the current stateTime
 		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
 		spriteBatch.begin();
-		spriteBatch.draw(currentFrame, 50, 50,400,400); // Draw current frame at (50, 50)
+		spriteBatch.draw(currentFrame, posX, 50,400,400); // Draw current frame at (50, 50)
 		spriteBatch.end();
 	}
 
@@ -121,5 +147,26 @@ public class firstAnimation extends ApplicationAdapter implements ApplicationLis
 			System.out.println("ERROR:"+error.toString());
 			return false;
 		}
+	}
+	protected int virtual_joystick_control() {
+		// iterar per multitouch
+		// cada "i" és un possible "touch" d'un dit a la pantalla
+		for(int i=0;i<10;i++)
+			if (Gdx.input.isTouched(i)) {
+				Vector3 touchPos = new Vector3();
+				touchPos.set(Gdx.input.getX(i), Gdx.input.getY(i), 0);
+				// traducció de coordenades reals (depen del dispositiu) a 800x480
+				camera.unproject(touchPos);
+				if (up.contains(touchPos.x, touchPos.y)) {
+					return UP;
+				} else if (down.contains(touchPos.x, touchPos.y)) {
+					return DOWN;
+				} else if (left.contains(touchPos.x, touchPos.y)) {
+					return LEFT;
+				} else if (right.contains(touchPos.x, touchPos.y)) {
+					return RIGHT;
+				}
+			}
+		return IDLE;
 	}
 }
